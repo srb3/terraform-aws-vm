293da40a457ef8bb7c48459f8d3d4b40a1ad3500
srb3 <sbrown@chef.io>
2020-09-01 18:11:02 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "ec2_instance" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "~> 2.13.0"
+  name                        = var.name
+  instance_count              = var.instance_count
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  monitoring                  = var.monitoring
+  vpc_security_group_ids      = var.security_group_ids
+  subnet_id                   = var.subnet_id
+  subnet_ids                  = var.subnet_ids
+  user_data                   = var.user_data
+  user_data_base64            = var.user_data_base64
+  root_block_device           = var.root_block_device
+  ebs_block_device            = var.ebs_block_device
+  ephemeral_block_device      = var.ephemeral_block_device
+  associate_public_ip_address = var.associate_public_ip_address
+  tags                        = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,92 @@
+variable "name" {
+  description = "The name to give this ec2 instance or group of instances"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "The number of instnaces to create"
+  type        = number
+  default     = 1
+}
+
+variable "ami" {
+  description = "ID of AMI to use for the instance/s"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
+  type        = string
+  default     = ""
+}
+
+variable "monitoring" {
+  description = "If true, the launched EC2 instance will have detailed monitoring enabled"
+  type        = bool
+  default     = false
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to associate with"
+  type        = list(string)
+  default     = null
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+  default     = ""
+}
+
+variable "subnet_ids" {
+  description = "A list of VPC Subnet IDs to launch in"
+  type        = list(string)
+  default     = []
+}
+
+variable "user_data" {
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument"
+  type        = string
+  default     = null
+}
+
+variable "user_data_base64" {
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument"
+  type        = string
+  default     = null
+}
+
+variable "root_block_device" {
+  description = "Customize details about the root block device of the instance. See Block Devices below for details"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ebs_block_device" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ephemeral_block_device" {
+  description = "Customize Ephemeral (also known as Instance Store) volumes on the instance"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "associate_public_ip_address" {
+  description = "If true, the EC2 instance will have associated public IP address"
+  type        = bool
+  default     = null
+}
+
+variable "tags" {
+  description = "mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
